7e7566bfe28fc96628036e529be7e5e8cb33485b
iamlegendharsh <harshwillwin1by1@gmail.com>
2026-03-07 21:11:32 +0530

    fix API_URL undefined in api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'https://food-app-4-e91k.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5173';
+
+const API = axios.create({
+  baseURL: API_URL,
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
