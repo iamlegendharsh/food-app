bd523a2f12131111fc2279d588e13185d57b7738
iamlegendharsh <harshwillwin1by1@gmail.com>
2026-03-07 06:53:09 +0530

    fix: define API axios instance

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-//const API = axios.create({ baseURL: 'http://localhost:5000/api' });
-const API_URL = "https://food-app-wis9.onrender.com"
+const API_URL = "https://food-app-wis9.onrender.com";
 
-API.interceptors.request.use(req => {
+const API = axios.create({ baseURL: API_URL });
+
+API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
