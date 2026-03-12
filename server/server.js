a9eeb2e671268e5577cd6cabe5536a54de1e131a
Harsh Vardhan <harshwillwin1by1@gmail.com>
2026-03-12 13:56:46 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,8 @@ app.use(cors({
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({limit : '10mb'}));
+app.use(express.urlencoded({limit: '10mb' , extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Backend is running');
